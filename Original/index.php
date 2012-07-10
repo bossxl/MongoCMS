2f5500aa35acd6209142f3c78b3a4935480c549e
Paul Boss <bosspj@gmail.com>
2012-07-10 00:07:30 -0600

    Added CakePHP and MongoDB Driver
    Moved the original stuff into the Orginal Folder so I can play a little
    with CakePHP.
    Need to Start to figure out how to build out the models and the
    interactions next. But from the looks of it, Cake seems the way to go.
    Much testing is still needed!

---
+++ Original/index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+// echo "Hello World";
+// phpinfo();
+require_once "connector.php";
+require_once "dbObjects/superDBObject.php";
+require_once "viewController.php";
+require_once "controller.php";
+?>
